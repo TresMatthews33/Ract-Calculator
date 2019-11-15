4fba566bc1cc6613b9399dc61e96c78e742efb8e
Tres Matthews <tres.matthews7@gmail.com>
2019-11-15 12:06:01 -0600

    Fixed issues in Js and added in some basic colors/styling to the calculator.

---
+++ src/components/Screen/Screen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Screen.css';
+import ResultsScreen from '../ResultsScreen/ResultsScreen';
+import ComputationScreen from '../ComputationScreen/ComputationScreen';
+
+/* Component that holds the results screen and calculation screen */
+class Screen extends Component {
+    render() {
+        return (
+            <div className='Screen'>
+                <ResultsScreen>{this.props.result}</ResultsScreen>
+                <ComputationScreen>{this.props.equation}</ComputationScreen>
+            </div>
+        );
+    }
+}
+
+export default Screen;
